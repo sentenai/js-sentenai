4bd6bcbceed0b965752f8df025d8131061c66e4c
brandly <brandly13@gmail.com>
2019-09-04 14:29:23 -0400

    expose more classes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { default as Client } from './src/api.js';
+import { default as Client, Stream, Field, View, Pattern } from './src/api.js';
 import * as flare from './src/flare.js';
 
 export default {
   Client,
+  Stream,
+  Field,
+  View,
+  Pattern,
   flare
-}
+};
